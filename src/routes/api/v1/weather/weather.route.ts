45690997a3c61748b58e6e17bd6f606a69549083
rzmsv <rmussavi@gmail.com>
2025-08-02 16:43:12 +0330

    feat: add authorization middleware to weather APIs

--- src/routes/api/v1/weather/weather.route.ts
+++ src/routes/api/v1/weather/weather.route.ts
@@ -22,14 +22,14 @@ const weatherController = new WeatherController(weatherService)
 /*                          Prefix: /api/v1/weather/                          */
 /* -------------------------------------------------------------------------- */
 /* ----------------------------------- GET ---------------------------------- */
-router.get("/", asyncHandler(userAuth), asyncHandler(validateDto(AddWeatherTypeRequest)), asyncHandler(weatherController.weatherList_controller))
+router.get("/", asyncHandler(weatherController.weatherList_controller))
 router.get("/:id", asyncHandler(weatherController.weatherById_controller))
 router.get("/latest/:cityName", asyncHandler(weatherController.weatherByCityName_controller))
 /* ---------------------------------- POST ---------------------------------- */
-router.post("/", asyncHandler(weatherController.addCurrentWeatherInDB_controller))
+router.post("/", asyncHandler(userAuth), asyncHandler(validateDto(AddWeatherTypeRequest)), asyncHandler(weatherController.addCurrentWeatherInDB_controller))
 /* ----------------------------------- PUT ---------------------------------- */
-router.put("/:id", asyncHandler(validateDto(UpdateWeatherRequest)), asyncHandler(weatherController.weatherUpdateInformation_controller))
+router.put("/:id", asyncHandler(userAuth), asyncHandler(validateDto(UpdateWeatherRequest)), asyncHandler(weatherController.weatherUpdateInformation_controller))
 /* --------------------------------- DELETE --------------------------------- */
-router.delete("/:id", asyncHandler(weatherController.weatherDeleteInformation_controller))
+router.delete("/:id", asyncHandler(userAuth), asyncHandler(weatherController.weatherDeleteInformation_controller))
 
 export default router
